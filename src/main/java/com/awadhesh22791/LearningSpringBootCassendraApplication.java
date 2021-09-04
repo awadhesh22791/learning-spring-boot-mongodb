6994ebe21040340b97bf726973e16c02a58e9bd2
Awadhesh Kumar <awadhesh22791@gmail.com>
2021-09-04 10:19:54 +0530

    Initial commit

---
+++ src/main/java/com/awadhesh22791/LearningSpringBootCassendraApplication.java
@@ -0,0 +1,13 @@
+package com.awadhesh22791;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringBootCassendraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringBootCassendraApplication.class, args);
+	}
+
+}
